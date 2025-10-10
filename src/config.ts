ca5ccc4918c6c2362763a04c5dc3fc1943ed7307
sleepysunflower <ufojwk@gmail.com>
2025-10-10 14:29:38 -0400

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,8 @@
-// src/config.ts
 export const DATA_BASE = './data';
-
 export const PMTILES = {
   incidents: `${DATA_BASE}/pmtiles/incidents.pmtiles`,
   lisa: `${DATA_BASE}/pmtiles/lisa_occurrence.pmtiles`
 };
-
 export const PARQUET = {
   seriesMonthly: `${DATA_BASE}/parquet/series_monthly.parquet`,
   seriesYearly: `${DATA_BASE}/parquet/series_yearly.parquet`,
@@ -13,5 +10,4 @@ export const PARQUET = {
   matrixDowMonth: `${DATA_BASE}/parquet/matrix_dow_month.parquet`,
   incidentVars: `${DATA_BASE}/parquet/incident_vars.parquet`
 };
-
 export const BASEMAP = `${DATA_BASE}/basemap/MTL_quartier.geojson`;
